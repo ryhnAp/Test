00b1b8d4e2a56f9bb7d2633aac96295a7901ff06
ryhnAp <ryhnap@gmail.com>
2023-12-10 00:51:49 +0330

    1 survived mutation remains :.( from engine :(((

--- src/test/java/domain/OrderTest.java
+++ src/test/java/domain/OrderTest.java
@@ -5,10 +5,14 @@ import com.google.gson.GsonBuilder;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -58,5 +62,49 @@ class OrderTest {
     @Test
     void GIVEN_order_string_objects_WHEN_equals_THEN_not_instance_of_order() {
         assertFalse(order1.equals(""));
+    }
+
+    @ParameterizedTest
+    @MethodSource("populateFraudulentQuantity")
+    void GIVEN_diff_history_minus1_scenario_WHEN_get_customer_fraudulent_quantity_THEN_returns_related_quantity(Order o, int id, int price) {
+        //setup
+        //exercise
+        //verify
+        assertEquals(id, o.getId());
+        assertEquals(price, o.getPrice());
+        //teardown
+    }
+
+    private static Stream<Arguments> populateFraudulentQuantity() {
+        Order o1 = new Order();
+        o1.setId(4);
+        o1.setCustomer(1);
+        o1.setPrice(100);
+        o1.setQuantity(-100);
+
+        Order o2 = new Order();
+        o2.setId(5);
+        o2.setCustomer(1);
+        o2.setPrice(0);
+        o2.setQuantity(-100);
+
+        Order o3 = new Order();
+        o3.setId(6);
+        o3.setCustomer(1);
+        o3.setPrice(1);
+        o3.setQuantity(-100);
+
+        Order o4 = new Order();
+        o4.setId(7);
+        o4.setCustomer(1);
+        o4.setPrice(-1);
+        o4.setQuantity(-100);
+
+        return Stream.of(
+                Arguments.of(o1, 4, 100),
+                Arguments.of(o2, 5, 0),
+                Arguments.of(o3, 6, 1),
+                Arguments.of(o4, 7, -1)
+        );
     }
 }
